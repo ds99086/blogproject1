e14ba4a32d787fbdc4c0c77f44ee12450d600798
nber106 <nber106@aucklanduni.ac.nz>
2021-05-26 10:00:21 +1200

    making the names better

--- TestDirectory/JSON_Templates_For_Project.js
+++ TestDirectory/JSON_Templates_For_Project.js
@@ -7,44 +7,52 @@
 const user = {
         userID: 1,
         username: 'uniqueUsername',
-        fname: 'userFirstName',
-        lname: 'userLastName',
-        birthday: '1901-06-18',
-        description: 'This is an example user'
+        userFname: 'userFirstName',
+        userLname: 'userLastName',
+        userDOB: '1901-06-18',
+        userAvatarID: 'Avatar001.png',
+        userDescription: 'This is an example user'
     };
 
 /*
 * ARTICLE JSON DOCUMENTATION
-* We have two types of JSon object for article, to 
-*
+* We have two types of JSon object for article, to enhance speed when we only need article information.
+* I'm not sure yet what to store in the artilce content, it could just be a long string, or it could be it's own JSON object.
 * 
 */
 //article is the full json object for handling articles
 const article = {
     articleID: 1,
-    title: 'Title of article',
-    date: 2021-05-25,
-    authorID: 1,
-    bodyContent: articleObject
+    articleTitle: 'Title of article',
+    articlePubDate: '2021-05-25',
+    articleAuthorID: 1,
+    articleContent: articleObject
     //directLink: "urlToArticle" //(If we actually deploy this, it is useful to have a url for each article (e.g. for articles to be shared) and we should probably store this value in the json and database)
 };
 //articleInfo is a "lighter" version of the article json without the body contnet
 const articleInfo = {
     articleID: 1,
-    title: 'Title of article',
-    date: 2021-05-25,
-    authorID: 1,
+    articleTitle: 'Title of article',
+    articlePubDate: '2021-05-25',
+    articleAuthorID: 1,
     //directLink: "urlToArticle" //(If we actually deploy this, it is useful to have a url for each article (e.g. for articles to be shared) and we should probably store this value in the json and database)
 }
 
+
+/*
+* COMMENT JSON
+*this handles comments, not that this is one json obejct where we expect a lot of null values.
+*
+*/
+
 const comment = {
     commentID: 1,
-    date: '2021-05-27',
+    commentDate: '2021-05-27',
     commentText: 'this is the text inside the comment',
-    commentLevel: 0 //0 indicates a comment replies directly to an article, 1 to a comment, etc.
-    parentComment: null, //default to null, referes to a commentID if this comment replies to another comment
-    authoerID: 1, //corresponds to a user ID
-    parentArticleID: 1, //corresponds to an articleID
+    commentLevel: 0, //0 indicates a comment replies directly to an article, 1 to a comment, etc.
+    commentParent: null, //default to null, referes to a commentID if this comment replies to another comment
+    commentAuthorID: 1, //corresponds to a user ID
+    commentArticleID: 1, //corresponds to an articleID
 };
 
 /*
