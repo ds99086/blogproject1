db6d3518866ba386d877b8bb6b4a00495ef7311d
tuicaodan <diaoyiwei@gmail.com>
2021-05-28 10:00:44 +1200

    fix db issue(hopefully)

--- routes/application-routes.js
+++ routes/application-routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { verifyAuthenticated } = require("../middleware/auth-middleware.js");
+const userDao = require("../modules/user-dao");
 
 //const testDao = require("../modules/test-dao.js");
 const articleDao = require("../modules/article-dao.js");
--- sql/project-database-init-script.sql
+++ sql/project-database-init-script.sql
@@ -1,4 +1,42 @@
+/*
+ * COMPSCI 718 719 Group Project, ABY: Allie Lim, Nicholas Berg, Yiwei Diao
+ *
+ * SQLite Script to generate the database for our Blog webiste 
+
+* Relational Schema
+* users(userID, username, passwordFieldsToUpdate, firstName, lastName, dateOfBirth)
+* articles(articleID, title, publishDate, lastEditDate, bodyContentOrLinkToContent, *authorID*)
+* comments(commentID, commentDate, commentText, commentLevel, parentComment, *authorID*, *parentArticleID* )
+* votes(*userID*, *commentID*, voteValue)
+ */
+
+--Drop existing tables to reinitialise the data base.
+--Commented out for safety.
+
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS articles;
+DROP TABLE IF EXISTS users;
+
+
+CREATE TABLE users (
+	userID INTEGER NOT NULL PRIMARY KEY,
+	username VARCHAR(100),
+    passwordFieldToUpdate VARCHAR(100),
+    firstName VARCHAR(100),
+	lastName VARCHAR(100),
+	dateOfBirth DATE
+	);
+
+CREATE TABLE articles (
+	articleID INTEGER NOT NULL PRIMARY KEY,
+	title VARCHAR(100),
+    publishDate DATE,
+    lastEditDate DATE,
+    bodyContentOrLinkToContent VARCHAR(100),
+    authorID INTEGER,
+    FOREIGN KEY (authorID) REFERENCES users (userID)
+	);
 
 CREATE TABLE comments (
 	commentID INTEGER NOT NULL PRIMARY KEY,
@@ -11,6 +49,28 @@ CREATE TABLE comments (
     FOREIGN KEY (authorID) REFERENCES users (userID),
     FOREIGN KEY (parentArticleID) REFERENCES articles (articleID)
 	);
+
+CREATE TABLE votes (
+	userID INTEGER NOT NULL,
+	commentID INTEGER NOT NULL,
+	voteValue INTEGER,
+	PRIMARY KEY (userID,commentID),
+    FOREIGN KEY (userID) REFERENCES users (userID),
+	FOREIGN KEY (commentID) REFERENCES comments (commentID)
+	);
+	
+--users(userID, username, passwordFieldsToUpdate, firstName, lastName, dateOfBirth)
+INSERT INTO users VALUES
+   (1, 'enigmaCracker', 'password', 'Alan', 'Turing', '1912-06-23'),
+   (2, 'theAdmiral', 'password', 'Grace', 'Hopper', '1906-12-09'),
+   (3, 'don', 'password', 'Donald', 'Knuth', '1938-01-10');
+
+---articles(articleID, title, publishDate, lastEditDate, bodyContentOrLinkToContent, *authorID*)
+INSERT INTO articles VALUES
+   (1, 'How I beat the Nazis', '1950-04-18', '1950-04-19', 'This is some article text https://en.wikipedia.org/wiki/Alan_Turing', 1),
+   (2, 'Coding on Boats: A how to guide', '1960-08-08', '1960-08-08', 'This is some article text https://en.wikipedia.org/wiki/Grace_Hopper', 2),
+   (3, 'Why I quit email an dyou should too', '1984-03-28', '1984-03-28', 'This is some article text https://www-cs-faculty.stanford.edu/~knuth/email.html', 3);
+
 
 INSERT INTO comments VALUES
    (1, '2010-04-20', 'This is the first comment, no parent, article 1', 0, 0, 2, 1),
