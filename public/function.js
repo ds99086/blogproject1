869a0e6f7225913e52432dc8620662487151f9bc
tuicaodan <diaoyiwei@gmail.com>
2021-06-02 17:24:09 +1200

    fix the profile updating bugs; changed the avatar choice method

--- public/function.js
+++ public/function.js
@@ -123,15 +123,33 @@ window.addEventListener("load", async function () {
     //if it is on the user setting page
     if(document.querySelector(".user-profile")){
         const profileUpdateBtn = document.querySelector(".profile-update-div-btn")
+        
         const profileUpdateDiv = document.querySelector(".profile-update-div")
-        profileUpdateBtn.addEventListener('click', e=>{
+        const profileUpdateSubmitBtn = profileUpdateDiv.querySelector("button")
+        profileUpdateSubmitBtn.disabled=true
+        profileUpdateBtn.addEventListener('click', e => {
             profileUpdateDiv.classList.toggle("invisible-class");
-            if (e.target.innerText == "Thinking about changing your information?"){
-                e.target.innerText = "Never mind!"
+            if (e.target.innerText == "Thinking about changing your information?") {
+                e.target.innerText = "Never mind!"                
             } else if (e.target.innerText == "Never mind!"){
                 e.target.innerText = "Thinking about changing your information?"
             }
         })
+        //listen to the current password input
+        const user_current_password = profileUpdateDiv.querySelector("#current_password")
+        const user_current_password_alert = profileUpdateDiv.querySelector("#current_password_alert_message")
+        user_current_password.addEventListener('input', async function (event) {
+            if (await checkUserPassWordCorrect(user_current_password.value)) {
+                user_current_password_alert.innerHTML = `Great! Your password is good!`;
+                profileUpdateSubmitBtn.disabled = false;
+            } else {
+                user_current_password_alert.innerHTML = `Your password is wrong!`;
+                profileUpdateSubmitBtn.disabled = true;
+            }
+        });
+        choiceAvatar();
+
+        
     }
 
     //this this checking whether we are on the new account page
@@ -156,7 +174,11 @@ window.addEventListener("load", async function () {
                 create_account_button.disabled = true;
             }
         });
-        userAvatarChangeLiveUpdate();
+        //add listener to avatar gallery
+        //update avatars once user click on icon
+        choiceAvatar()       
+        
+
     };
 
 
@@ -398,17 +420,17 @@ async function retrieveArticleByArticleID(articleID) {
     return articleObj;
 }
 
-function userAvatarChangeLiveUpdate() {
-    //get user choice of avatar, show the avatar picture live in the div
-    const user_avatar_update_div = document.querySelector(".AvatarDiv");
-    const user_avatar_choice_input = document.querySelector("#avatarID");
-    user_avatar_update_div.innerHTML = `<img src="/images/Avatars/${user_avatar_choice_input.value}.png" class="avatarIcon">`
-    user_avatar_choice_input.onchange = function () { changeAvatarIcon() };
-    function changeAvatarIcon() {
-        let user_avatar_choice_input = document.querySelector("#avatarID");
-        user_avatar_update_div.innerHTML = `<img src="/images/Avatars/${user_avatar_choice_input.value}.png" class="avatarIcon">`
-    };
-};
+// function userAvatarChangeLiveUpdate() {
+//     //get user choice of avatar, show the avatar picture live in the div
+//     const user_avatar_update_div = document.querySelector(".AvatarDiv");
+//     const user_avatar_choice_input = document.querySelector("#avatarID");
+//     user_avatar_update_div.innerHTML = `<img src="/images/Avatars/${user_avatar_choice_input.value}.png" class="avatarIcon">`
+//     user_avatar_choice_input.onchange = function () { changeAvatarIcon() };
+//     function changeAvatarIcon() {
+//         let user_avatar_choice_input = document.querySelector("#avatarID");
+//         user_avatar_update_div.innerHTML = `<img src="/images/Avatars/${user_avatar_choice_input.value}.png" class="avatarIcon">`
+//     };
+// };
 
 //check whether the username has already taken
 //if the username exist, return true, otherwise return false
@@ -420,6 +442,18 @@ async function checkUsernameExist(username_input) {
     } else {
         return false;
     };
+};
+
+//when user want to update profile
+//check the current password input match with the password in database
+//if not show the warning and disable the update button
+async function checkUserPassWordCorrect(password_input) {
+    const user_input = password_input;
+    console.log(user_input)
+    let response = await fetch(`./checkUserPassword?input_password=${user_input}`);
+    let use_password_checking_obj = await response.json();
+    console.log(use_password_checking_obj.result)
+    return use_password_checking_obj.result;
 };
 
 //check the re-enter password is consist or not.
@@ -533,6 +567,24 @@ function replyButtonFunction(e) {
     } else {
         console.log("Error! Unexpcted button text.");
     }
+}
+
+function choiceAvatar(){
+    const avatarIcons = document.querySelectorAll(".avatar-gallery-icon")
+    document.querySelector(".avatar-gallery-div").addEventListener('click', e=>{
+        if (e.target && e.target.matches(".avatar-gallery-icon")){
+            const avatarFilename = e.target.getAttribute("avatarFileName")
+            const avatarID = avatarFilename.substring((avatarFilename.lastIndexOf("."))-2, avatarFilename.lastIndexOf("."))
+            document.getElementById("txtAvatarID").value = avatarID;
+            for(avatarIcon of avatarIcons){
+                if(avatarIcon == e.target){  
+                    avatarIcon.style.backgroundColor = "#ff0505"
+                } else {
+                    avatarIcon.style.backgroundColor = "transparent"
+                }
+            }
+        }
+    })
 }
 
 })
